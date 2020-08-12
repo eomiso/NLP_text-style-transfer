5a330f638105f0a1e143af0e69e38a0776393456
Uiseop Eom <ytrtef@gmail.com>
2020-08-12 23:54:13 +0900

    debug Encoder, still debugging Decoder
    [TODO]
    Issue the Vocab sharing problem between two domains

---
+++ style_transfer/style_transfer.py
@@ -0,0 +1,99 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Encoder(nn.Module):
+    def __init__(self, batch_size, embed_dim, dim_y, dim_z, dropout):
+        """
+        Required parameters:
+            batch_size, dim_z, dim_y, embed_dim, labels
+        """
+        super().__init__()
+        self.fc = nn.Linear( 1, dim_y) # output: (batch_size, dim_y)
+        self.init_z = torch.zeros(batch_size, dim_z)
+        
+        self.rnn = nn.GRU(embed_dim, dim_y + dim_z,num_layers=1, dropout=dropout)
+    
+    def forward(self, src, src_len, labels):
+        labels = labels.unsqueeze(-1) # (batch_size, 1), 엔코더 밖에서 해줘도 괜찮을 듯
+        packed_embed = nn.utils.rnn.pack_padded_sequence(src, src_len) # input input to rnn
+        
+        # initial hidden state of the encoder :cat ( y, z)
+        # QUESTION: y를 만드는 linear fuction 의 파라미터도 학습의 대상이 되는게 맞겠지?
+        init_hidden = torch.cat((self.fc(labels), self.init_z), -1) 
+        _, hidden = self.rnn(packed_embed, init_hidden.unsqueeze(0))
+        # hidden : hidden_state of the final time step
+        hidden = hidden.squeeze(0)
+        z = hidden[:, dim_y:]
+        return z
+
+
+class Generator(nn.Module):
+    def __init__(self,batch_size, embed_dim, embeddings, dim_y, dim_z, temperature):
+        """
+        Required Parameters:
+            all the same exept "z" which is the output from the Encoder
+        """
+        super().__init__() 
+        self.gamma = temperature
+        self.dim_h = dim_y + dim_z
+        self.embedding = embeddings
+        self.batch_size = batch_size
+
+        self.fc = nn.Linear(1, dim_y)
+        # The hidden state's dimension: dim_y + dim_z
+        self.rnn = nn.GRU(embed_dim, self.dim_h, num_layers=1)
+        # ISSUE : 두 개의 fc_out 이 필요한 것인가 -> 원본 코드에서는 동일한 vocab을 공유하는 듯 하다.
+        self.fc_out = nn.Linear(self.dim_h, self.embedding.num_embeddings) 
+
+    def forward(self, z, src, src_len, labels, transfered = True):
+        """
+        Required Parameters
+            z : output from the encoder
+            src, src_len : for teacher forcing the original sentence
+
+        z, labels, 를 이용해서 inital hidden을 Genrator 앞에다가 넣어줘야 한다.
+        """
+        #QUESTION: Packed padded sequence를 decoder부분에서도 써줘야 하는가?
+        #QUESTION: Max_len 어떻게 처리할래?
+        # unroll은 어디까지? end_of_token까지 인가? # 원래 코드는 max_seq 만큼 time step 진행
+        labels = labels.unsqueeze(-1)  # (batch_size, 1)
+
+        outputs = torch.zeros(*src.shape, self.dim_h)
+        predictions = torch.zeros(*src.shape, self.embedding.num_embeddings) # g_logits in original code
+        
+        if transfered:
+            # using softmax to feed previous decoding
+            h0 = torch.cat((self.fc(1-labels), z), -1)  #h0_transfered
+            
+            input = self.embedding(TRG.vocab['<sos>']).repeat(32,1) # <go> or <sos>
+            input = input.unsqueeze(0)
+            hidden = h0.unsqueeze(0) # [num_layers * num_directions = 1, batch, hidden_size]
+            for t in range(1, src_len): 
+                output, hidden = self.rnn(input, hidden)
+                outputs[t] = output
+                prediction = self.fc_out(output)
+                predictions[t] = prediction
+                # TODO: max 를 뽑아낼 수 있게 해야 한다??? 원본코드의 softsample_word를 참조
+                input = (F.gumbel_softmax(prediction) / self.gamma) * self.embedding.weights
+            
+
+        else:
+            h0 =  torch.cat((self.fc(labels), z), -1)  #h0_original
+            # using teacher forcing
+            input = self.embedding(src[0]).unsqueeze(0)    
+            hidden = h0.unsqueeze(0) # [num_layers * num_directions = 1, batch, hidden_size]
+            for t in range(1,max(src_len)):
+                print(input.shape)      
+                output, hidden = self.rnn(input, hidden)
+                outputs[t] = output 
+                prediction = self.fc_out(output)
+                predictions[t] = prediction # predictions are for calculating loss_rec
+                input = self.embedding(src[t]).unsqueeze(0)
+            
+        outputs = torch.concat((h0, outputs), 1) # according to the paper you need h0 in the sequence to feed the discriminator
+
+        return outputs, predictions
---
+++ style_transfer/test.py
@@ -0,0 +1,125 @@
+import unittest
+from style_transfer import Encoder
+import requests
+
+import os
+import re
+
+import torch.nn as nn
+import torchtext
+from konlpy import init_jvm
+from konlpy.tag import Okt
+
+
+class TestModules(unittest.TestCase):
+
+    if not os.path.exists('1000sents.csv'):
+
+        # Get file link from google drive
+        file_id = "1R_XGHvkgMArQM6SM59_U7qLaDD2gb3FZ"
+        file_download_link = "https://docs.google.com/uc?export=download&id=" + file_id
+
+        # Download and unzip file
+        data = requests.get(file_download_link)
+        filename = '1000sents.csv'
+        with open(filename, 'wb') as f:
+            f.write(data.content)
+
+
+    # 전처리 
+
+    """
+    토큰화를 위한 형태소분석기를 정의해줍니다.
+    """
+    init_jvm()
+    okt = Okt() 
+    stop_words = [  # 불용어를 정의합니다.
+        '은', '는', '이', '가', '하', '아', '것', '들', '의', '있', '되', '수',
+        '보', '주', '등', '한', '을', '를'
+    ]
+    def tokenize(text):
+        """
+        code modified from https://github.com/reniew/NSMC_Sentimental-Analysis
+        """
+        # 1. 한글 및 공백을 제외한 문자 모두 제거.
+        review_text = re.sub("[^가-힣ㄱ-ㅎㅏ-ㅣ\\s]", "", text)
+
+        # 2. okt 객체를 활용해서 형태소 단위로 나눈다.
+        word_review = Okt.morphs(review_text, stem=True)
+
+        # 3. 불용어 제거
+        word_review = [token for token in word_review if not token in stop_words]
+
+        return word_review
+
+
+    """
+    torchtext의 데이터셋 형태로 변환하기 위해,
+    데이터의 각 필드를 정의해줍니다.
+    """
+    from torchtext import data
+
+    KOR = data.Field(       # 한국어 문장
+        tokenize=tokenize,
+        init_token='<sos>', # 문장의 시작 토큰
+        eos_token='<eos>',  # 문장의 끝 토큰
+        include_lengths=True
+    )
+    ENG = data.Field(       # 영어 문장
+        tokenize='spacy',
+        init_token='<sos>',
+        eos_token='<eos>',
+        lower=True,
+        include_lengths=True
+    )
+
+    train_data_kor = data.TabularDataset(
+        path='1000sents.csv',
+        format='csv',
+        fields=[('korean', KOR)],
+        skip_header=True
+    )
+
+    train_data_eng = data.TabularDataset(
+        path='1000sents.csv',
+        format='csv',
+        field=[('english', ENG)]
+    )
+    
+    KOR.build_vocab(train_data0, min_freq=3)
+    ENG.build_vocab(train_data1, min_freq=3)
+    
+
+    batch_size = 32
+    dim_y = 10
+    dim_z = 30
+    embed_dim = 100
+    dropout = 0.1
+
+    embedding_kor = nn.Embedding(len(KOR.vocab),embed_dim)
+    embedding_eng = nn.Embedding(len(ENG.vocab),embed_dim)
+
+    train_iterator_eng = data.BucketIterator(
+        train_data_eng, 
+        batch_size=batch_size,
+        sort_within_batch=True,
+        sort_key=lambda x : len(x.english),
+    )
+
+    def test_encoder(self):
+        sample, sample_len = next(iter(train_iterator_eng))
+        labels = torch.ones(batch_size)
+
+        encoder = Encoder(batch_size, embed_dim, dim_y, dim_z, dropout)
+        
+        size = encoder(embedding_eng(sample), sample_len, labels).shape
+        assert size == torch.Size((batch_size, dim_z))
+
+    def test_decoder(self):
+        pass
+    def test_discriminator(self):
+        pass
+
+
+if __name__=="__main__":
+    unittest.main()
