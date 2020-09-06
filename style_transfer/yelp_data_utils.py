eb9b73d064767daaaa449b7f40c97705243d212d
Uiseop Eom <ytrtef@gmail.com>
2020-09-06 14:17:59 +0900

    Refactor yelp_data_utils.py
    [Details]
    Use tabulardataset.splits to get train, val, test iter \
    Make TEXT_field saver & loader using dill package
    Make ReversibleField(custumed) to use instead of Field for reversing
    from tokenization.
    
    [Improvements to be made in the future]
    How to reverse tokenize "spacy"?
    
    
    Former-commit-id: ef76b6f6e2aa85b1e040580988db3b7ee8bd6005

--- style_transfer/yelp_data_utils.py
+++ style_transfer/yelp_data_utils.py
@@ -6,7 +6,93 @@ author : Uiseop Eom
 
 
 import pandas as pd
-from tqdm.tqdm import tqdm
+from tqdm import tqdm
+
+import torchtext.data as data
+import torch
+import glob
+
+
+def get_iterator_for_train_val_test(path_to_texts, batch_size):
+    #path_to_texts should include "/" at the end
+    # Equalize the number of sentences in the two files.
+    # 혹시 Dataloader 만들 때 이거 같이 해주었었는지?
+    
+    equalize_seq_num(*[f for f in glob.glob(path_to_texts+"*train*")])
+    equalize_seq_num(*[f for f in glob.glob(path_to_texts+"*dev*")])
+    equalize_seq_num(*[f for f in glob.glob(path_to_texts+"*test*")])
+
+
+    # Initiate Text Field 
+    TEXT = ReversibleField(
+        tokenize="spacy",
+        #preprocessing=preprocessing,
+        init_token = '<sos>',
+        eos_token = '<eos>',
+        include_lengths=True,
+        lower = True
+    )
+
+    # Init TabularDataset
+    train_0, val_0, test_0 = data.TabularDataset.splits(
+        path=path_to_texts,
+        train="sentiment.train.0",
+        validation="sentiment.dev.0",
+        test="sentiment.test.0",
+        format='tsv',
+        fields=[('src',TEXT)],
+    )
+    train_1, val_1, test_1 = data.TabularDataset.splits(
+        path=path_to_texts,
+        train="sentiment.train.1",
+        validation="sentiment.dev.1",
+        test="sentiment.test.1",
+        format='tsv',
+        fields=[('src',TEXT)]
+    )
+    ## Build Vocab vector saved in .vector_cache(?)
+    # if embedding:
+    #    TEXT.build_vocab(dataset0, dataset1, min_freq=3, vectors=embedding)
+    #else:
+    
+    # 1.9M vocab 1.25 GB 가즈아
+    TEXT.build_vocab(train_0, train_1, min_freq=5, vectors="glove.42B.300d")
+    # for <unk> tokens
+    TEXT.vocab.unk_init = torch.randn
+    
+    ## make iterator
+    train_iter_0, val_iter_0, test_iter_0=BucketIterator_complete_last(
+        (train_0, val_0, test_0), 
+        batch_size=(batch_size, 256, 256)
+        sort_within_batch=True,
+        sort_key=lambda x : len(x.src),
+    )
+
+    train_iter_1, val_iter_1, test_iter_1=BucketIterator_complete_last(
+        (train_1, val_1, test_1), 
+        batch_size=b(batch_size, 256, 256)
+        sort_within_batch=True,
+        sort_key=lambda x : len(x.src)
+    )
+    
+    print("Vocab Size: {} tokens.".format(len(TEXT.vocab)))
+    save_vocab(TEXT_field.vocab)
+
+    return (train_iter_0, val_iter_0, test_iter_0), (train_iter_1, val_iter_1, test_iter_1), TEXT
+
+import pickle
+def save_vocab(vocab):
+    """
+    for loading embeddings in transfer.py,
+    you need to form a dummy nn.Embedding first
+    """
+    with open("YELP.vocab", 'wb') as f:
+        pickle.dump(vocab, f)
+
+def load_vocab():
+    with open("YELP.vocab", 'rb') as f:
+        vocab = pickle.load(f)
+    return vocab
 
 def equalize_seq_num(path_0, path_1):
     """
@@ -25,6 +111,7 @@ def equalize_seq_num(path_0, path_1):
     print("smaller text length: " + str(len(smaller)))
     print("larger text length: " + str(len(larger)))
 
+    print("... Equalizing {} and {}".format(path_0, path_1))
     repeat_num = int( (len(larger) - len(smaller)) / len(smaller))
     print(repeat_num)
     remain_num = (len(larger)-len(smaller)) % len(smaller)
@@ -81,6 +168,36 @@ class BucketIterator_complete_last(data.BucketIterator): # return last batch of 
             yield minibatch
         if minibatch:
             yield minibatch 
+
+
+class ReversibleField(data.Field):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    
+    def reverse(self, batch):
+        if not self.batch_first:
+            batch = batch.t()
+        with torch.cuda.device_of(batch):
+            batch = batch.tolist()
+        batch = [[self.vocab.itos[ind] for ind in ex] for ex in batch]  # denumericalize
+
+        def trim(s, t):
+            sentence = []
+            for w in s:
+                if w == t:
+                    break
+                sentence.append(w)
+            return sentence
+
+        batch = [trim(ex, self.eos_token) for ex in batch]  # trim past frst eos
+        print(batch)
+        def filter_special(tok):
+            return tok not in (self.init_token, self.pad_token)
+        batch = [list(filter(filter_special, ex)) for ex in batch]
+        print(batch)
+
+        return [' '.join(ex) for ex in batch]
+
 
 def preprocessing(text):
     if text[-1]==".":
